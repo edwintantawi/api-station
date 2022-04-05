e4342b09cc5332ede282d6e921aaad7d4758590e
edwintantawi <edwintantawi@gmail.com>
2022-04-05 08:26:47 +0700

    💄 Add credit and action button on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,21 @@
-import { Stack, Typography, useMediaQuery } from '@mui/material';
+import {
+  Button,
+  Link,
+  Paper,
+  Stack,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
 
 // MUI Icons
 import EvStationIcon from '@mui/icons-material/EvStation';
+import { useAppDispatch } from '../hooks/redux.hook';
+import { toggleDrawer } from '../redux/slices/ui.slice';
 
 const HomePage = () => {
-  const matches = useMediaQuery('(min-width: 900px)');
+  const matchesMd = useMediaQuery('(min-width: 900px)');
+  const matchesSm = useMediaQuery('(min-width: 600px)');
+  const dispatch = useAppDispatch();
 
   return (
     <Stack
@@ -12,12 +23,12 @@ const HomePage = () => {
       alignItems="center"
       justifyContent="center">
       <Stack
-        direction={matches ? 'row' : 'column'}
+        direction={matchesMd ? 'row' : 'column'}
         alignItems="center"
         columnGap={2}>
         <EvStationIcon sx={{ fontSize: '100px' }} />
         <Typography
-          variant={matches ? 'h3' : 'h4'}
+          variant={matchesMd ? 'h3' : 'h4'}
           textAlign="center"
           component="h2"
           fontWeight="bold">
@@ -27,24 +38,32 @@ const HomePage = () => {
       <Typography variant="body2" component="p" textAlign="center">
         list of thousands of usable <strong>Public API's</strong> for developers
       </Typography>
+      <Paper variant="outlined" sx={{ px: 4, py: 2, mt: 2 }}>
+        <Stack>
+          <Typography variant="body2" component="p" textAlign="center">
+            [ Frontend Build by ]{' '}
+            <Link href="https://edwintantawi.vercel.app/" target="__blank">
+              Edwin Tantawi
+            </Link>
+          </Typography>
 
-      {/* <Typography variant="body2" component="p" mt={2}>
-        Create and build with love by Edwin Tantawi
-      </Typography> */}
-      {/* <ul>
-        <li>
-          [ Frontend Build by ]{' '}
-          <Link href="https://edwintantawi.vercel.app/" target="__blank">
-            Edwin Tantawi
-          </Link>
-        </li>
-        <li>
-          [ APIs source by ]{' '}
-          <Link href="https://api.publicapis.org/" target="__blank">
-            PublicAPIs
-          </Link>
-        </li>
-      </ul> */}
+          <Typography variant="body2" component="p" textAlign="center">
+            [ APIs source by ]{' '}
+            <Link href="https://api.publicapis.org/" target="__blank">
+              PublicAPIs
+            </Link>
+          </Typography>
+        </Stack>
+      </Paper>
+      {!matchesSm ? (
+        <Button
+          variant="contained"
+          disableElevation
+          sx={{ marginTop: 2 }}
+          onClick={() => dispatch(toggleDrawer())}>
+          Get started
+        </Button>
+      ) : null}
     </Stack>
   );
 };
